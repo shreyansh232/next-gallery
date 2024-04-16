92c3c7521e5ff5a71e20b7f30ef0cc63141673e2
shreyansh232 <shreyansh487@gmail.com>
2024-04-16 23:43:56 +0530

    scaffold UI

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,18 @@ export const metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+function TopNav(){
+  return (
+    <nav className="flex items-center justify-between w-full p-4 text-xl font-semibold border-b ">
+      <div >Gallery</div>
+
+      <div>Sign In</div>
+      
+    </nav>
+  );
+
+}
+
 export default function RootLayout({
   children,
 }: {
@@ -20,7 +32,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`font-sans ${inter.variable}`}>{children}</body>
+      <body className={`font-sans ${inter.variable} flex flex-col gap-4`}>
+        <TopNav />
+        {children}
+        </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
 import Link from "next/link";
 
+
+const mockUrls = [
+  "https://utfs.io/f/5f6f4063-264b-4105-8947-faa611aac82c-1t9xah.png",
+  "https://utfs.io/f/33f53e1b-621b-43ea-a89a-6e2bfec9982e-kxfbj6.png",
+  "https://utfs.io/f/36db8f1a-e779-4997-ba6b-8d54c1c24a78-y5pedl.jpg",
+  "https://utfs.io/f/23224830-5cf7-47b0-8c92-707a49d244ab-oo8xtv.12.44.jpeg",
+
+];
+
+const mockImages = mockUrls.map((url, index) => ({
+  id: index + 1,
+  url,
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      Hello Gallery
+    <main className="">
+      <div className="flex flex-wrap gap-4">
+        {[...mockImages, ...mockImages, ...mockImages, ...mockImages].map((image) => (
+          <div key={image.id} className="w-48">
+            <img src={image.url} className="w-full h-full object-cover" />
+          </div>
+        ))}
+      </div>
     </main>
   );
 }
