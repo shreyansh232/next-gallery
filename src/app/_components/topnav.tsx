2a0c2130936a89cdc07479ffecab3e2c40c90bdb
shreyansh232 <shreyansh487@gmail.com>
2024-04-17 15:09:47 +0530

    Auth

---
+++ src/app/_components/topnav.tsx
@@ -0,0 +1,21 @@
+import { SignOutButton, SignedOut, SignedIn, SignInButton, UserButton } from "@clerk/nextjs";
+
+export function TopNav(){
+    return (
+      <nav className="flex items-center justify-between w-full p-4 text-xl font-semibold border-b ">
+        <div >Gallery</div>
+  
+        <div>
+            <SignedOut>
+                <SignInButton />
+            </SignedOut>
+            <SignedIn>
+                <UserButton/>
+            </SignedIn>
+        </div>
+        
+      </nav>
+    );
+  
+  }
+  
