d7b3072834bb5be0b3a06e5aea2c6a7707a92a87
shreyansh232 <shreyansh487@gmail.com>
2024-04-20 00:47:59 +0530

    Custom Upload Button

--- src/app/_components/topnav.tsx
+++ src/app/_components/topnav.tsx
@@ -1,33 +1,20 @@
-"use client";
-
 import { SignedOut, SignedIn, SignInButton, UserButton } from "@clerk/nextjs";
-import { UploadButton } from "../utils/uploadthing";
-import { useRouter } from "next/navigation";
+import { SimpleUploadButton } from "./simple-upload-button";
 
-export function TopNav(){
+export function TopNav() {
+  return (
+    <nav className="flex w-full items-center justify-between border-b p-4 text-xl font-semibold ">
+      <div>Gallery</div>
 
-
-    const router = useRouter();
-    return (
-      <nav className="flex items-center justify-between w-full p-4 text-xl font-semibold border-b ">
-        <div >Gallery</div>
-  
-        <div className="flex flex-row">
-            <SignedOut>
-                <SignInButton />
-            </SignedOut>
-            <SignedIn>
-                <UploadButton endpoint="imageUploader"
-                onClientUploadComplete={() => {
-                    router.refresh();
-                }}
-                />
-                <UserButton/>
-            </SignedIn>
-        </div>
-        
-      </nav>
-    );
-  
-  }
-  
+      <div className="flex flex-row items-center gap-4">
+        <SignedOut>
+          <SignInButton />
+        </SignedOut>
+        <SignedIn>
+          <SimpleUploadButton />
+          <UserButton />
+        </SignedIn>
+      </div>
+    </nav>
+  );
+}
