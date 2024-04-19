54872c866fb969a99d22ea6aa71413c98aaf9cc1
shreyansh232 <shreyansh487@gmail.com>
2024-04-20 04:05:52 +0530

    Rate limit

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -3,6 +3,7 @@ import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { UploadThingError } from "uploadthing/server";
 import { db } from "~/server/db";
 import { images } from "~/server/db/schema";
+import { ratelimit } from "~/server/ratelimit";
  
 const f = createUploadthing();
 
@@ -12,13 +13,9 @@ export const ourFileRouter = {
       const user = auth();
       if (!user.userId) throw new UploadThingError("Unauthorized");
 
-      // const fullUserData = await clerkClient.users.getUser(user.userId);
+      const { success } = await ratelimit.limit(user.userId);
 
-      // if (fullUserData?.privateMetadata?.["can-upload"] !== true)
-      //   throw new UploadThingError("User Does Not Have Upload Permissions");
-
-      // const { success } = await ratelimit.limit(user.userId);
-      // if (!success) throw new UploadThingError("Ratelimited");
+      if (!success) throw new UploadThingError("Ratelimited");
 
       return { userId: user.userId };
     })
